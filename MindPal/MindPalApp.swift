c85f208c60f0387e973c3c6d24fe05a26f014f14
Oncu <oohancan@gmail.com>
2024-11-19 22:15:47 +0300

    Initial Commit

---
+++ MindPal/MindPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindPalApp.swift
+//  MindPal
+//
+//  Created by Oncu Can on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MindPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
